e6a95a9c25b8e4e048beb8c630a4450b3128e827
Wilson Tellez <wetelleza@unal.edu.co>
2017-02-11 14:17:58 -0500

    route to aboutUS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  root to: "pages#index"
-
+  root to: 'pages#index'
+  get 'aboutUS', to: 'pages#aboutUS'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
