3eecec7c70e270c63ce6a53a74d1fdc098f86176
Cristian <cdcordobaa@unal.edu.co>
2017-02-13 00:46:26 +0000

    stashed changes on messages validatios

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+    validates :tittle, :description, presence: true
+    validates :description, length: {minimum: 10}
+    validates :tittle, uniqueness: true
 end
